52b535e918a2c2aaa36feb2bf2bb79ab6cb073ec
Sanderson <134319317+SandersonFTJunior@users.noreply.github.com>
2023-07-28 12:15:21 -0300

    ex008 e ex009

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 50
+
+console.log(`A velocidade do seu caro é ${vel}km/h`)
+if (vel >60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança')
+
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'Brasil'
+if (pais == 'Brasil'){
+    console.log('Oi meu chapa')
+}else{
+    console.log('Hello gringo')
+}
