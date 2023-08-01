f85430e06ba84899fd25923947f46022ad056c1c
Sanderson <134319317+SandersonFTJunior@users.noreply.github.com>
2023-08-01 18:26:27 -0300

    corrigindo erro

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,15 +1,14 @@
 function carregar(){
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('foto')
-//var data = new Date()
-//var hora = data.getHours()
-var hora = 20
+var data = new Date()
+var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 6 && hora < 12){
-    img.src = 'imagem/foto-manha.jpg'
+    img.src = './imagens/foto-manha.jpg'
 }else if (hora >= 12 && hora < 18){
-    img.src = 'imagem/foto-tarde.jpg'
+    img.src = './imagens/foto-tarde.jpg'
 }else{
-    img.src = 'imagem/foto-noite.jpg'
+    img.src = './imagens/foto-noite.jpg'
 }
 }
