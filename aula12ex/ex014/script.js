cd2c906d8dd4d0968c2943885e612e60e2d4f146
Sanderson <134319317+SandersonFTJunior@users.noreply.github.com>
2023-08-02 18:40:25 -0300

    cores

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -6,9 +6,12 @@ var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 6 && hora < 12){
     img.src = './imagens/foto-manha.jpg'
+    document.body.style.background = '#e9cbb3'
 }else if (hora >= 12 && hora < 18){
     img.src = './imagens/foto-tarde.jpg'
+    document.body.style.background = '#ff9b04'
 }else{
     img.src = './imagens/foto-noite.jpg'
+    document.body.style.background = '#715679'
 }
 }
