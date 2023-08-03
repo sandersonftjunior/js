9382d38340243497494bcecf14ad9c0d8500c7bb
Sanderson <134319317+SandersonFTJunior@users.noreply.github.com>
2023-08-03 19:22:20 -0300

    ex015

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -10,8 +10,33 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement ('img')
+        img.setAttribute ('id', 'foto')
         if (fsex[0].checked){
-
+            genero = 'homen'
+            if (idade >= 0 && idade < 15){
+                img.setAttribute ('src', 'imagens/bebe-h.jpg')
+            }else if (idade < 21){
+                img.setAttribute ('src', 'imagens/jovem-h.jpg')
+            } else if(idade < 50){
+                img.setAttribute ('src', 'imagens/adulto-h.jpg')
+            }else{
+                img.setAttribute ('src', 'imagens/velho-h.jpg')
+            }
+        }else{
+            genero = 'mulher'
+            if (idade >= 0 && idade < 15){
+                img.setAttribute ('src', 'imagens/bebe-m.jpg')
+            }else if (idade < 21){
+                img.setAttribute ('src', 'imagens/jovem-m.jpg')
+            } else if(idade < 50){
+                img.setAttribute ('src', 'imagens/adulto-m.jpg')
+            }else{
+                img.setAttribute ('src', 'imagens/velho-m.jpg')
+            }
         }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
