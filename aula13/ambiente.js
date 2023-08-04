9f69764b56448b8d57b66dde106860f22e261787
Sanderson <134319317+SandersonFTJunior@users.noreply.github.com>
2023-08-04 14:58:53 -0300

    aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+while(c <= 100){
+    console.log(`Tudo bem? ${c}`)
+    c++
+}
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
