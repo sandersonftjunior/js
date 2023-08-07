89aaf27f6d70561a3737009ceb52b4ddbf30c8d2
Sanderson <134319317+SandersonFTJunior@users.noreply.github.com>
2023-08-07 19:48:26 -0300

    tentativa de contador

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,34 @@
-window.alert('Olá')
+function cont(){
+
+
+var ini = document.getElementById('ini')
+var fim = document.getElementById('fim')
+var passo = document.getElementById('passo')
+var res = document.getElementById('res')
+
+    if(ini.length == 0 ||fim.length == 0 || passo.length ==0){
+    res.innerHTML = ('Impossivel calcular, tente novamente.')
+} else if(passo <0){
+    res.innerHTML = ('Números negativos não são aceitos, tente novamente')
+}
+
+var i = Number(ini);
+var f = Number(fim);
+var p = Number(passo);
+
+if (p <=0){
+    res.innerHTML = ('Invalido')
+}else if(i < f){
+    for(i;i <= f; i += p){
+        res.innerHTML += `${i} 👉`;
+    }
+}else{
+    for(i;i >= f; i -= p)
+    res.innerHTML += `${i} 👉`;
+
+}
+{
+    res.innerHTML += '✋';
+
+}
+}
