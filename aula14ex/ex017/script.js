5ab174b2ef570779662a6a1f86b764593bedaa12
Sanderson <134319317+SandersonFTJunior@users.noreply.github.com>
2023-08-09 19:48:48 -0300

    ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,16 @@
-window.alert('Olá')
+function tabuada(){
+    var n = document.getElementById('num')
+    var t = document.getElementById('tabuada')
+    if(num.value.length == 0){
+        alert('Inválido, digite um número')
+    }else{
+    var n = Number(num.value)
+    tabuada.innerHTMl = ''
+
+    for(var c = 1; c <= 10; c++){
+        var item = document.createElement
+        item.text = `${n} x ${c} = ${n * c}`
+        tabuada.appendChild('item');
+    }
+    }
+}
